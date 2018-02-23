b7711df2fc4c3c36645a2eed7809108767b04f2e
darshan <darshan.gowda008@gmail.com>
2018-02-24 00:21:33 +0530

    basic set up done

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/ngsw-worker.js');
+    }
+  })
   .catch(err => console.log(err));
