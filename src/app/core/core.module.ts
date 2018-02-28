0f7f8a4a630ad72bd8aa70518e4f28df0e1832c3
unknown <arunmanju.09@gmail.com>
2018-02-28 17:46:07 +0530

    Notifications

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './auth.service';
+import { NotificationService } from './notification.service';
 import { AuthGuard } from './auth.guard';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { DataService } from './data-service.service';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth }     from 'angularfire2/auth';
 
 @NgModule({
   imports: [
@@ -11,6 +14,6 @@ import { DataService } from './data-service.service';
     AngularFireAuthModule
   ],
   declarations: [],
-  providers: [AuthService, AuthGuard, DataService]
+  providers: [AuthService, NotificationService, AuthGuard, DataService, AngularFireDatabase, AngularFireAuth]
 })
 export class CoreModule { }
