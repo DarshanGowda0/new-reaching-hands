55c352585a7ff0c61fd73dad4da6ee62339b4b65
darshan <darshan.gowda008@gmail.com>
2018-02-28 22:24:20 +0530

    example charts added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { DataService } from './data-service.service';
+import { GoogleChartResolver } from './google-chart-resolver';
 
 @NgModule({
   imports: [
@@ -11,6 +12,6 @@ import { DataService } from './data-service.service';
     AngularFireAuthModule
   ],
   declarations: [],
-  providers: [AuthService, AuthGuard, DataService]
+  providers: [AuthService, AuthGuard, DataService, GoogleChartResolver]
 })
 export class CoreModule { }
