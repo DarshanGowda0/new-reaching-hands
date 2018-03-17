ee0617dff19cb07c851935c85fb019a1f3119463
Vachana Shetty <vachanagshetty@gmail.com>
2018-03-17 20:47:08 +0530

    3rd graph

--- src/app/components/common/summary-report/summary-report.component.ts
+++ src/app/components/common/summary-report/summary-report.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../../../core/data-service.service';
-import { tap } from 'rxjs/operators';
+import { tap,map } from 'rxjs/operators';
 import { forEach } from '@firebase/util';
 
 export interface CostModel {
@@ -21,6 +21,7 @@ export class SummaryReportComponent implements OnInit {
   @Input() google: any;
   costData = [];
   temp: string = null;
+  totalCost: number = 0;
   categoryList = ['Inventory', 'Services', 'Maintenance', 'Education'];
   costComp: number[] = [0, 0, 0, 0];
   logTypeOptions = ['Added', 'Issued', 'Donated'];
@@ -31,15 +32,19 @@ export class SummaryReportComponent implements OnInit {
     this.google = this.route.snapshot.data.google;
 
     this.dataService.getSummary()
-      // .pipe(
-      //   tap(logs => {
-
-      //   })
-      // )
+      .pipe(
+        map(logs => {
+            logs.forEach(item=>{
+              item.date = this.dateFormat(item.date);
+            })
+            return logs;
+        })
+      )
       .subscribe(logs => {
         this.dataService.getAllItems().subscribe(items => {
           this.computeCost(logs);
           this.computeCost1(logs, items);
+          this.computeCost2(logs);
         });
       });
 
@@ -127,15 +132,75 @@ export class SummaryReportComponent implements OnInit {
       });
     });
 
+
+
     dataArray.sort(function (a, b) {
       return b.costObject.total - a.costObject.total;
     });
 
-    for (let i = 0; i < this.categoryList.length; i++) {
-      this.costData.push([this.categoryList[i], this.costComp[i]]);
-    }
-    this.drawChart1(dataArray, nameHash);
+    //console.log('array',dataArray[1]);
 
+    const arrayTen = new Array();
+    if (arrayTen.length > 10) {
+      for (let i = 0; i < 10; i++) {
+        arrayTen[i] = dataArray[i];
+      }
+      this.drawChart1(arrayTen, nameHash);
+    }
+    else {
+      this.drawChart1(dataArray, nameHash);
+    }
+    console.log('array', arrayTen[1]);
+
+    // for (let i = 0; i < this.categoryList.length; i++) {
+    //   this.costData.push([this.categoryList[i], this.costComp[i]]);
+    // }
+
+
+  }
+
+  computeCost2(val) {
+    this.costData = [];
+    
+    const myhash = new Map();
+
+    val.forEach(element => {
+      if (myhash.has(element.date)) {
+        if (element.logType !== this.logTypeOptions[1]) {
+         
+          this.totalCost += element.cost;
+          myhash.set(element.date, this.totalCost);
+        }
+      } else {
+        if (element.logType !== this.logTypeOptions[1]) {
+          this.totalCost = element.cost;
+          myhash.set(element.date, this.totalCost);
+        }
+      }
+    });
+
+    console.log('booooooooooootrre',myhash);
+
+    const dataArray1 = new Array();
+    myhash.forEach((value, key) => {
+      dataArray1.push({
+        'date': key,
+        'cost': value
+      });
+    });
+
+
+    dataArray1.forEach(element => {
+      const row = [];
+  
+        row.push(this.dateFormat(element.date));
+        row.push(element.cost);
+        row.push(element.cost - 300);
+        row.push(element.cost + 300);
+        this.costData.push(row);
+      
+    });
+    this.drawCostChart2();
   }
 
   compare(a, b) {
@@ -169,7 +234,7 @@ export class SummaryReportComponent implements OnInit {
 
   drawChart1(dataArray, nameHash) {
 
-    console.log('name ', nameHash);
+    console.log('name ', dataArray);
 
     const myData = [];
     myData.push(['Item', 'Total', 'Purchased', 'Donated']);
@@ -198,4 +263,45 @@ export class SummaryReportComponent implements OnInit {
     const chart = new this.google.charts.Bar(document.getElementById('barchart_values'));
     chart.draw(data, options);
   }
+
+
+  drawCostChart2() {
+    const data = new this.google.visualization.DataTable();
+    data.addColumn('string', 'x');
+    data.addColumn('number', 'cost');
+    data.addColumn({ id: 'i0', type: 'number', role: 'interval' });
+    data.addColumn({ id: 'i1', type: 'number', role: 'interval' });
+
+    data.addRows(this.costData);
+
+    // The intervals data as narrow lines (useful for showing raw source data)
+    const options_lines = {
+      title: 'Consumption rate',
+      curveType: 'function',
+      lineWidth: 4,
+      intervals: { 'style': 'area' },
+      animation: {
+        duration: 1000,
+        easing: 'in',
+        startup: true
+      },
+      pointSize: 7,
+      dataOpacity: 0.3,
+      height: 360,
+      series: {
+        1: { lineDashStyle: [4, 1] }
+      },
+      colors: ['#e2431e', '#4374e0'],
+    };
+
+    const chart_lines = new this.google.visualization.LineChart(document.getElementById('costChart'));
+    chart_lines.draw(data, options_lines);
+  }
+
+  dateFormat(date) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return String(date.toLocaleString('en-US')).substr(0,9);
+  }
+
+
 }
